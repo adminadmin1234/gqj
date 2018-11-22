2c2164c633c19be02243cc5c0bd1508f43b193ab
wb-lzh405564 <wb-lzh405564@alibaba-inc.com>
2018-11-22 14:12:01 +0800

    1

--- app/web/component/layout/index/header/header.vue
+++ app/web/component/layout/index/header/header.vue
@@ -23,22 +23,6 @@
             </div>
         </div>
     </nav>
-    <header class="intro-header" style="background-color:#3c763d;">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-                    <div class="site-heading">
-                        <div>
-                            <img src="../../../../asset/images/loading.gif" width="80px" height="80px" style="display: inline">
-                        </div>
-                        <h1>博客</h1>
-                        <hr class="large">
-                        <span class="subheading">IBlog</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
 </div>
 </template>
 <script type="babel">
