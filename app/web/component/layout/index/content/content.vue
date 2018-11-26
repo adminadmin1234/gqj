0bc78a8dbac65d54d0936bfb1ae05d2a5aa96098
liandadada <359734657@qq.com>
2018-11-26 23:14:01 +0800

    modify home

--- app/web/component/layout/index/content/content.vue
+++ app/web/component/layout/index/content/content.vue
@@ -1,6 +1,63 @@
 <template>
-  <div>
-    <slot name='content'></slot>
+  <div class="content-wrap">
+    <div class="content-left">
+      <div class="content-up">
+        <img src="../../../../asset/images/head-pic.jpg" class="head-pic" alt="">
+        <h1 class="site-name">世界知最</h1>
+        <hr class="content-left-hr">
+      </div>
+      <ul class="content-down">
+        <li>分享web前端技术</li>
+        <li>探索灰色赚钱套路</li>
+        <li>收集常用工具文档</li>
+      </ul>
+      <div class="content-desc">你是一只钟爱代码的猿吗！？</div>
+      <div class="content-desc">你是否有那么一瞬是enjoy这个职业的！？</div>
+      <div class="content-desc">你曾拒绝过产品的需求吗！？</div>
+    </div>
+    <div class="content-right">
+      <div class="code-wrap">
+        <img src="../../../../asset/images/code.png" alt="">
+        <p>扫一扫有惊喜！</p>
+      </div>
+      <div class="content-wrap">
+        <div>热门推荐</div>
+        <ul class="content-wrap-ul">
+          <li>
+            <img src="../../../../asset/images/code.png" alt="" class="content-li-picture">
+            <div class="content-detail-wrap">
+              <p>你是否有那么一瞬是enjoy这个职业的！？</p>
+              <span class="content-detail-date">2018-07-11</span>
+              <span class="content-detail-label">jquery</span>
+            </div>
+          </li>
+          <li>
+            <img src="../../../../asset/images/code.png" alt="" class="content-li-picture">
+            <div class="content-detail-wrap">
+              <p>你是否有那么一瞬是enjoy这个职业的！？</p>
+              <span class="content-detail-date">2018-07-11</span>
+              <span class="content-detail-label">jquery</span>
+            </div>
+          </li>
+          <li>
+            <img src="../../../../asset/images/code.png" alt="" class="content-li-picture">
+            <div class="content-detail-wrap">
+              <p>你是否有那么一瞬是enjoy这个职业的！？</p>
+              <span class="content-detail-date">2018-07-11</span>
+              <span class="content-detail-label">jquery</span>
+            </div>
+          </li>
+          <li>
+            <img src="../../../../asset/images/code.png" alt="" class="content-li-picture">
+            <div class="content-detail-wrap">
+              <p>你是否有那么一瞬是enjoy这个职业的！？</p>
+              <span class="content-detail-date">2018-07-11</span>
+              <span class="content-detail-label">jquery</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <style>
