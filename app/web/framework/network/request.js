b0a4c139dfcde15caf9c614c574418d8df772fbb
wb-lzh405564 <wb-lzh405564@alibaba-inc.com>
2019-01-04 19:03:41 +0800

    modify

--- app/web/framework/network/request.js
+++ app/web/framework/network/request.js
@@ -1,35 +1,3 @@
-// 'use strict';
-// import axios from 'axios';
-// const ip = require('ip');
-// const localIP = document.domain;// ip.address();
-// console.log('localIP', localIP);
-// axios.defaults.baseURL = `http://${localIP}:80`;
-// axios.defaults.timeout = 15000;
-// axios.defaults.xsrfHeaderName = 'x-csrf-token';
-// axios.defaults.xsrfCookieName = 'csrfToken';
-// export default {
-//   post(url, json, store = {}) {
-//     const { state = { origin: '' } } = store;
-//     const headers = {};
-//     if (EASY_ENV_IS_NODE) {
-//       headers['x-csrf-token'] = state.csrf;
-//       headers.Cookie = `csrfToken=${state.csrf}`;
-//     }
-//     if (`${state.origin}` === 'undefined') {
-//       return axios.post(`${url}`, json, { headers });
-//     } else {
-//       return axios.post(`${state.origin}${url}`, json, { headers });
-//     }
-//   },
-//   get(url, store = {}) {
-//     const { state = { origin: '' } } = store;
-//     if (`${state.origin}` === 'undefined') {
-//       return axios.get(`${url}`);
-//     } else {
-//       return axios.get(`${state.origin}${url}`);
-//     }
-//   }
-// };
 'use strict';
 import axios from 'axios';
 // axios.defaults.baseURL = 'http://127.0.0.1:7001';
