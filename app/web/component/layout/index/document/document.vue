5d2ba7f9e4daab9e7d607e47dbe3356017d16173
wb-lzh405564 <wb-lzh405564@alibaba-inc.com>
2019-02-21 18:58:11 +0800

    feat:modify

--- app/web/component/layout/index/document/document.vue
+++ app/web/component/layout/index/document/document.vue
@@ -1,31 +1,57 @@
 <template>
-<layout :title="dataRes.seo.title" :description="dataRes.seo.description" :keywords="dataRes.seo.keywords">
-       <div class='main'>
-          <LayoutHeader :activeStr="document"></LayoutHeader>
-          <Document :dataRes="dataRes"></Document>
-          <LayoutFooter :footerPosition="footerPosition"></LayoutFooter>
-       </div>
-</layout>
+    <div>
+        <div class="menu-nav-wrap-full" v-bind:class="{ 'menu-nav-wrap-full-h2' : menuShow}">
+                    <div class="menu-nav-wrap">
+                        <div class="menu-single-wrap" v-bind:class="{ 'menu-single-wrap-active' : index===0}" @click="menuToLoad(item.lb_id,index)" v-for="(item,index) in dataRes.labelList.list" v-on:mouseenter="menuSingleShow($event)" v-on:mouseleave="menuSingleHide($event)">
+                        {{item.lb_name}}
+                        </div>
+                    </div>
+                </div>
+                <div class="content-wrap">
+                    <div class="search-con">
+                        <input type="text" class="search-text" v-model="keyword">
+                        <div @click="search" class="search-sub">搜索</div>
+                    </div>
+                    <div class="content-single-wrap">
+                        <div class="content-single" v-for="(item,index) in dataRes.articleList.temp" v-on:mouseenter="dataMiddle($event)" v-on:mouseleave="hiddenMiddle($event)">
+                        <a :href="item.atc_id | addHref">
+                            <img :src="item.atc_content | imgUrlFun" class="content-single-preview" :alt="item.atc_title">
+                            <div class="content-single-middle">
+                            <span class="content-single-middle-span"><i class="el-icon-view">{{item.atc_preview}}</i></span>
+                            <span class="content-single-middle-span content-single-middle-span-mobile-like"><i class="el-icon-star-on">{{item.atc_like}}</i></span>
+                            <span class="content-single-middle-span"><i class="el-icon-download">{{item.atc_download}}</i></span>
+                            <span class="content-single-middle-span content-single-middle-span-mobile"><i class="el-icon-date">{{item.atc_publish_time | formatData}}</i></span>
+                            <a class="content-single-middle-a" v-if="item.atc_fileUrl != null" target="blanck" :href="item.atc_fileUrl | preview"><span @click="onPreview(item.atc_id)" class="content-single-middle-preview">预览</span></a>
+                            </div>
+                            <div class="content-single-footer">
+                            {{item.atc_title}}
+                            </div>
+                        </a>
+                        </div>
+                    </div>
+                    <div class="not-content-wrap" v-if="dataRes.articleList.total==0">
+                        <img class="not-content-img" src="../../../../asset/images/head-pic.png" alt="">
+                        <h2>暂无内容</h2>
+                    </div>
+                </div>
+            <div class="pagination_wrap" v-if="dataRes.articleList.total>0">
+                <PaginationPage @current-change="changePaginationIndex" :total="dataRes.articleList.total" :pageSize="pagination.pagesize"></PaginationPage>
+              </div>
+    </div>
 </template>
 <style scoped lang="scss">
-@import '../../asset/css/mixin.scss';
-@import "document.scss";
+    @import '../../../../asset/css/mixin.scss';
+    @import "./document.scss";
 </style>
 <script type="babel">
-  import '../../asset/js/rem.js';
-  import LayoutHeader from '../../component/layout/index/headercommon/headercommon';
-  import Document from '../../component/layout/index/document/document';
-  import LayoutFooter from '../../component/layout/index/footer/footer';
+  import $ from "jquery";
+  import '../../../../asset/js/rem.js';
   import format from 'date-fns/format'
-  import Vue from 'vue';
   import request from 'framework/network/request';
-
-  import PaginationPage from '../../component/layout/index/pagination/pagination';
+  import PaginationPage from '../pagination/pagination';
   export default {
     components: {
-      LayoutHeader,
-      LayoutFooter,
-      Document
+      PaginationPage
     },
     data(){
       return {
@@ -46,6 +72,7 @@
         document:'document',
       }
     },
+    props:['dataRes'],
     watch:{
     },
     mounted() {
@@ -64,7 +91,7 @@
     methods: {
       changePaginationIndex(currentIndex){
         this.pagination.index = currentIndex;
-        // this.loadData(this.labelId);
+        this.loadData(1);
       },
       onPreview(atcid){
           request.get(`/detail/api/article/countPreview?atcid=${atcid}`).then(response => {
@@ -103,7 +130,7 @@
           });
       },
       loadData(lbId) {
-        request.get(`/document/api/article/list?id=${lbId}&index=${this.pagination.index}&pagesize=${this.pagination.pagesize}`).then(response => {
+        request.get(`/document?id=${lbId}&index=${this.pagination.index}`).then(response => {
           this.articleList = response.data.temp;
           this.pagination.total = response.data.total;
         });
